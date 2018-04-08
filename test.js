460180db8a981463308a56f6c91b75febeb210f8
Oleksiy Chechel <alex.mirrr@gmail.com>
2018-04-08 09:56:20 +0300

    Add support of multiinstance require

--- test.js
+++ test.js
@@ -1,14 +1,38 @@
 // alert("!!!");
 (function(global) {
+    var alliance = global["goalliance"];
+    var extender = {
+        enumerable: false,
+        writable: false,
+        configurable: false,
+        value: function(ext) {
+            for (var e in ext) {
+                if (ext.hasOwnProperty(e)) {
+                    this[e] = ext[e];
+                }
+            }
+        },
+    }
 
-    var uris = {
+    if (typeof alliance === "undefined") {
+        alliance = {
+            uris: {},
+            modules: {},
+            exports: {},
+            circular: {},
+        }
+        Object.defineProperty(alliance.uris, "extend", extender);
+        Object.defineProperty(alliance.modules, "extend", extender);
+    }
+
+    alliance.uris.extend({
         "main": "main.js",
         "test1": "test1.js",
         "test2": "test2/index.js",
         "test3": "test3.js",
         "test4": "test4.js",
-    };
-    var modules = {
+    });
+    alliance.modules.extend({
         "main": function(require, exports, module, define) {
             module.dependencies = ["test1"];
             module.init = function() {
@@ -40,54 +64,40 @@
             try {
                 module.init = function() {
                     console.log("init test4");
-                    require("test1");
                 }
                 module.dependencies = []; 
-                require("main");
             }  catch (err) {     
-                throw '"'+uris["test4"]+'": '+err.stack;
+                throw '"'+alliance.uris["test4"]+'": '+err;
             }
         },
-    }
+    });
 
-    var exports = {};
-    var circular = {};
+
 
     var require = function (path) {
         if (typeof path !== "string") {
             return
         }
         path = path.replace(/-/g, '_').replace(/.js$/g, '').toLowerCase();
-        if (exports[path] !== undefined) {
-            return exports[path];
+        if (typeof alliance.exports[path] !== "undefined") {
+            return alliance.exports[path];
         }
 
-        if (circular[path]) {
+        if (alliance.circular[path]) {
             throw "Module '"+path+"' has circular dependencies!";
         }
 
         var module = {
             id: path,
-            uri: uris[path],
+            uri: alliance.uris[path],
             exports: {},
             dependencies: [], 
         };
-        Object.defineProperty(module.exports, "extend", {
-            enumerable: false,
-            writable: false,
-            configurable: false,
-            value: function(exports) {
-                for (var e in exports) {
-                    if (exports.hasOwnProperty(e)) {
-                        this[e] = exports[e];
-                    }
-                }
-            },
-        });
+        Object.defineProperty(module.exports, "extend", extender);
 
-        if (modules[path] !== undefined) {
-            circular[path] = true;
-            modules[path](require, module.exports, module);
+        if (typeof alliance.modules[path] !== "undefined") {
+            alliance.circular[path] = true;
+            alliance.modules[path](require, module.exports, module);
             
             if (Array.isArray(module.dependencies) && module.dependencies.length>0) {
                 for (var i in module.dependencies) {
@@ -100,15 +110,15 @@
                 try {
                     module.init();   
                 }  catch (err) {
-                    throw '"'+uris[path]+'": function init(): '+err.stack
+                    throw '"'+alliance.uris[path]+'": function init(): '+err
                 }
             }
-            exports[path] = module.exports; 
+            alliance.exports[path] = module.exports; 
             return module.exports;
         }
     }
 
-    if (require("main") === undefined) {
+    if (typeof require("main") === "undefined") {
         console.error("Module 'main' is not defined")
     }
 
